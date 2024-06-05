383d7adc2ec9f44fbc1aa33b079ee107cc623d17
DropEntry <admin@dropentry.com>
2024-06-05 14:39:03 +1000

    Updates

---
+++ precache-manifest.2d3717536dbb5bb3803bf9632cf63e01.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f014411808091e3c88f6bf94af0279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "551046fed29fe243ea0e",
+    "url": "/static/css/main.a16280ca.chunk.css"
+  },
+  {
+    "revision": "50c6435254bb41f165ea",
+    "url": "/static/js/2.3b3c4410.chunk.js"
+  },
+  {
+    "revision": "137742d1824a900b7e1b9020ddee17fb",
+    "url": "/static/js/2.3b3c4410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "551046fed29fe243ea0e",
+    "url": "/static/js/main.313a0e8c.chunk.js"
+  },
+  {
+    "revision": "8e03037278040721348c",
+    "url": "/static/js/runtime-main.3aade4d2.js"
+  },
+  {
+    "revision": "ce58df747e4f5a5a24de5214274d3c7c",
+    "url": "/static/media/fork-icon.ce58df74.svg"
+  },
+  {
+    "revision": "37d5908dc78c990de67476170ee51443",
+    "url": "/static/media/github-logo.37d5908d.svg"
+  },
+  {
+    "revision": "7543a22b9c83ce3a6818621fdd2e8420",
+    "url": "/static/media/plus-icon.7543a22b.svg"
+  },
+  {
+    "revision": "12a6659c20228155488d5a75ae893fdf",
+    "url": "/static/media/share-icon.12a6659c.svg"
+  }
+]);
